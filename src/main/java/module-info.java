2237da44f083f52b79349d8e42cfc4775b5d2f3d
siannone16-iann <s.iannone16@studenti.unisa.it>
2025-11-21 11:56:31 +0100

    Repo inzio

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.mycompany.biblioteca {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.biblioteca to javafx.fxml;
+    exports com.mycompany.biblioteca;
+}
