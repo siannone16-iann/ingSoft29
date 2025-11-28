44bb4e5eac396d802d434239bf106cb1a7431f50
Davidemart1 <davidemartelli2004@gmail.com>
2025-11-28 13:39:58 +0100

    correzione errore di prima(prima avevo fatto solo il commit di prova)

---
+++ src/main/java/it/unisa/diem/oop/bibliotecaprogetto/SystemInfo.java
@@ -0,0 +1,13 @@
+package it.unisa.diem.oop.bibliotecaprogetto;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/* doesn't work with source level 1.8:
+module it.unisa.diem.oop.bibliotecaprogetto {
+    requires javafx.controls;
+    exports it.unisa.diem.oop.bibliotecaprogetto;
+}
+*/
