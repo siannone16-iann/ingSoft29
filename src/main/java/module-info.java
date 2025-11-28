4ec1125614e5ed6e2b369573528fe4a0d52e24ae
siannone16-iann <s.iannone16@studenti.unisa.it>
2025-11-28 11:54:38 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/*
-module com.mycompany.biblioteca {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.biblioteca to javafx.fxml;
-    exports com.mycompany.biblioteca;
-}
-*/
