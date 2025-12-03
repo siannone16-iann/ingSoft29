7691610c33412d0fab1b8a4feb798b9d41f33960
siannone16-iann <s.iannone16@studenti.unisa.it>
2025-12-03 20:17:35 +0100

    Implementazione classe Libro.java

--- src/main/java/Libro.java
+++ src/main/java/Libro.java
@@ -1,7 +1,36 @@
+
 /**
  *
  * @author simoneiannone
  */
 
 public class Libro {
+
+    private final String Titolo;
+    private int copie;
+    private int copiePrestate;
+    private final int isbn;
+    private final int annoProduzione;
+
+    public Libro(String titolo, int isbn, int annoProduzione, int copie, int copiePrestate) {
+        Titolo = titolo;
+        this.isbn = isbn;
+        this.annoProduzione = annoProduzione;
+        this.copie = copie;
+        this.copiePrestate = 0;
+    }
+
+    public boolean isAvailable(){
+        if (copiePrestate < copie) return true;
+        return false;
+    }
+
+    public boolean prestito(){
+        if(isAvailable()){
+            copiePrestate++;
+            return true;
+        }
+        else return false;   //gestione eccezione da valutare se farlo in BibliotecaManager
+    }
+
 }
